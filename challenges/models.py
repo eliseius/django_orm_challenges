7c71a60d53f64c346895480d79e0a22420ef026c
eliseius <133158187+eliseius@users.noreply.github.com>
2023-10-11 22:35:27 +0300

    Level 2 (#3)
    * Done
    
    * Corrected
    
    * Corrected
    
    * Level 2 task A
    
    * Done level_2 task B
    
    * Corrected
    
    * Corrected

--- challenges/models.py
+++ challenges/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import json
+
 
 class Book(models.Model):
     title = models.CharField(max_length=256)
@@ -8,3 +10,64 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Laptop(models.Model):
+    class Meta:
+        db_table = 'Laptop_info'
+        get_latest_by = 'created_at'
+        ordering = ['-created_at']
+
+    brand = models.CharField(max_length=120)
+    year_of_issue = models.DateField()
+    amount_of_ram = models.PositiveSmallIntegerField()
+    hd_capacity = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    quantity_in_stock = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def to_json(self):
+        data = {
+            'brand': self.brand,
+            'year_of_issue': self.year_of_issue.strftime('%Y'),
+            'amount_of_ram': self.amount_of_ram,
+            'hd_capacity': self.hd_capacity,
+            'price': self.price,
+            'quantity_in_stock': self.quantity_in_stock,
+            'created_at':self.created_at.strftime('%d-%m-%Y %H:%M:%S')
+        }
+
+        return json.dumps(data)
+    
+
+class PostBlog(models.Model):
+    class Meta:
+        get_latest_by = 'created_at'
+        ordering = ['-created_at']
+
+    MESSAGE_STATUS_CHOICES = [
+        ('pb', 'published'),
+        ('npb', 'not published'),
+        ('ban', 'banned')
+    ]
+    
+    title = models.CharField(max_length=120)
+    text_post = models.CharField(max_length=1500)
+    author_name = models.CharField(max_length=256)
+    status = models.CharField(max_length=3, choices=MESSAGE_STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    publication_date = models.DateTimeField()
+    category = models.CharField(max_length=120)
+
+    def to_json(self):
+        data = {
+            'title': self.title,
+            'text_post': self.text_post,
+            'author_name': self.author_name,
+            'status': self.status,
+            'publication_date': self.publication_date.strftime('%d-%m-%Y %H:%M:%S'),
+            'category': self.category,
+            'created_at':self.created_at.strftime('%d-%m-%Y %H:%M:%S'),
+        }
+
+        return json.dumps(data)
